5963c868242b08e8f4b6a067b90fe650565e8939
Debapratim Bhattacharya <debabhattacharya@deloitte.com>
2017-03-04 13:10:57 +1100

    Foundation project added

--- gulp-config.js
+++ gulp-config.js
@@ -1,10 +1,10 @@
 module.exports = function() {
-  var instanceRoot = "C:\\websites\\Habitat.dev.local";
+    var instanceRoot = "D:\\inetpub\\wwwroot\\Habitat.dev.local";
   var config = {
     websiteRoot: instanceRoot + "\\Website",
     sitecoreLibraries: instanceRoot + "\\Website\\bin",
     licensePath: instanceRoot + "\\Data\\license.xml",
-    solutionName: "Sitecore.Demo.Group",
+    solutionName: "Habitat",
     buildConfiguration: "Debug",
     runCleanBuilds: false
   };
