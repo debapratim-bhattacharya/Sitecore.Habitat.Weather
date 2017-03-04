55b484baecb3abd8c3d4184b55f2f35a860efdb3
Debapratim Bhattacharya <debabhattacharya@deloitte.com>
2017-03-04 19:12:56 +1100

    Basic agent creation - done

---
+++ src/Feature/Weather/code/Scheduler/Agent/WeatherBasedNotifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Feature.Weather.Scheduler.Agent
+{
+    public class WeatherBasedNotifier
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
