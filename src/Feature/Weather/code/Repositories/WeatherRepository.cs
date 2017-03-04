f4877482e3a62749e5206368f1855fc743ed284a
gaurav1143 <gaurav.sde13@gmail.com>
2017-03-04 19:15:19 +0530

    Weather request data model

---
+++ src/Feature/Weather/code/Repositories/WeatherRepository.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Foundation.Weather.Models;
+using Sitecore.Mvc.Presentation;
+using System.Web.Script.Serialization;
+using System.Net.Http;
+using System.Net;
+using Sitecore.Feature.Weather.Models;
+
+namespace Sitecore.Feature.Weather.Repositories
+{
+    public class WeatherRepository : IWeatherRepository
+    {
+        static HttpClient httpClient = new HttpClient();       
+        
+        public string URL = string.Empty;
+
+        public static string json = string.Empty;
+
+
+        public string GetCurrentWeatherJson(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                json = client.DownloadString(url);
+            }
+            WeatherCurrent WeatherCurrent = (new JavaScriptSerializer()).Deserialize<WeatherCurrent>(json);
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            json = js.Serialize(WeatherCurrent);
+            return json;
+        }
+        public string GetForecastWeatherJson(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                json = client.DownloadString(url);
+            }
+            WeatherInfo WeatherForecast = (new JavaScriptSerializer()).Deserialize<WeatherInfo>(json);
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            json = js.Serialize(WeatherForecast);
+            return json;
+        }
+
+        string IWeatherRepository.GetWeatherByCity(WeatherRequestModel request)
+        {
+            URL = request.BaseURL + request.RelativeURL;
+            Dictionary<string, string> replacements = new Dictionary<string, string>
+            {
+                { "{{city}}", request.location },
+                {"{{appid}}",request.AppId },
+                {"{{metric}}",request.Unit }
+            };
+            foreach (string to_replace in replacements.Keys)
+            {
+                URL = URL.Replace(to_replace, replacements[to_replace]);
+            }
+            return GetCurrentWeatherJson(URL);
+        }
+
+
+        string IWeatherRepository.GetWeatherForecastByCity(WeatherRequestModel request)
+        {
+            URL = request.BaseURL + request.RelativeURL;
+            Dictionary<string, string> replacements = new Dictionary<string, string>
+            {
+                { "{{city}}", request.location },
+                {"{{appid}}",request.AppId },
+                {"{{metric}}",request.Unit }
+            };
+            foreach (string to_replace in replacements.Keys)
+            {
+                URL = URL.Replace(to_replace, replacements[to_replace]);
+            }
+            return GetCurrentWeatherJson(URL);
+        }
+    }
+}
+}
--- src/Feature/Weather/code/Scheduler/Agent/WeatherBasedNotifier.cs
+++ src/Feature/Weather/code/Scheduler/Agent/WeatherBasedNotifier.cs
@@ -1,15 +1,44 @@
-﻿using System;
+﻿using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Foundation.Weather.Models;
+using Sitecore.Mvc.Presentation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 
 namespace Sitecore.Feature.Weather.Scheduler.Agent
 {
     public class WeatherBasedNotifier
     {
+        static HttpClient httpClient = new HttpClient();
+        private Item GetDatasourceItem()
+        {
+            var datasourceId = RenderingContext.Current.Rendering.DataSource;
+            return ID.IsID(datasourceId) ? Context.Database.GetItem(datasourceId) : null;
+        }
         public void Run()
         {
+            var currentItem = GetDatasourceItem();
+            var weatherInfo = new WeatherCurrent();
 
+            var apiSource = (LookupField)currentItem.Fields["ApiSource"];
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+         
+
+            using (httpClient)
+            {
+                HttpResponseMessage response = httpClient.GetAsync(apiSource.TargetItem.Uri.Path).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = response.Content.ReadAsStringAsync().Result;
+                    weatherInfo = Newtonsoft.Json.JsonConvert.DeserializeObject<WeatherCurrent>(data);
+                };
+            }
         }
     }
 }
